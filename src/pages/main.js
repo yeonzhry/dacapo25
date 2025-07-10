260fd1679491c9bfc0ab0acbbf4f6437d45312da
이연재 <yeonzhry@gmail.com>
2025-07-10 21:25:03 +0900

    이미지 리사이징

--- src/pages/main.js
+++ src/pages/main.js
@@ -5,7 +5,7 @@ import styles from '../styles/main.module.css';
 
 const Main = () => {
   const navigate = useNavigate();
-  const [showOnboard, setShowOnboard] = useState(true);
+  const [showOnboard] = useState(true);
   const fullText = 'Highway to';
   const [text, setText] = useState('');
   const [index, setIndex] = useState(0);
@@ -57,7 +57,7 @@ const Main = () => {
         </div>
         {showStartButton && (
           <img
-            src="/images/start.png"
+            src="/images/start.webp"
             alt="Start"
             className={styles.start}
             onClick={handleStart}
@@ -66,33 +66,33 @@ const Main = () => {
       </div>
 
       <img src="/images/bg.webp" alt="bg" className={styles.bg} />
-      <img src="/images/dacapo2.png" alt="bg" className={styles.dacapo} loading="lazy"/>
-      <img src="/images/img1.webp" alt="img1" className={styles.img1} loading="lazy"/>
-      <img src="/images/img2.webp" alt="img2" className={styles.img2} loading="lazy"/>
-      <img src="/images/img3.webp" alt="img3" className={styles.img3} loading="lazy"/>
-      <img src="/images/img4.webp" alt="img4" className={styles.img4} loading="lazy"/>
-      <img src="/images/img5.webp" alt="img5" className={styles.img5} loading="lazy"/>
-      <img src="/images/img6.webp" alt="img6" className={styles.img6} loading="lazy"/>
-      <img src="/images/img7.webp" alt="img7" className={styles.img7} loading="lazy"/>
-      <img src="/images/img8.webp" alt="img8" className={styles.img8} loading="lazy"/>
-      <img src="/images/img9.webp" alt="img9" className={styles.img9} loading="lazy"/>
-      <img src="/images/img10.webp" alt="img10" className={styles.img10} loading="lazy"/>
-      <img src="/images/img11.webp" alt="img11" className={styles.img11} loading="lazy"/>
-      <img src="/images/img12.webp" alt="img12" className={styles.img12} loading="lazy"/>
-      <img src="/images/img13.webp" alt="img13" className={styles.img13} loading="lazy"/>
-      <img src="/images/img14.webp" alt="img14" className={styles.img14} loading="lazy"/>
-      <img src="/images/img15.webp" alt="img15" className={styles.img15} loading="lazy"/>
-      <img src="/images/img16.webp" alt="img16" className={styles.img16} loading="lazy"/>
-      <img src="/images/img17.webp" alt="img17" className={styles.img17} loading="lazy"/>
-      <img src="/images/img18.webp" alt="img18" className={styles.img18} loading="lazy"/>
-      <img src="/images/img19.webp" alt="img19" className={styles.img19} loading="lazy"/>
-      <img src="/images/img20.webp" alt="img20" className={styles.img20} loading="lazy"/>
-      <img src="/images/img21.webp" alt="img21" className={styles.img21} loading="lazy"/>
-      <img src="/images/img22.webp" alt="img22" className={styles.img22} loading="lazy"/>
-      <img src="/images/img23.webp" alt="img23" className={styles.img23} loading="lazy"/>
-      <img src="/images/img24.webp" alt="img24" className={styles.img24} loading="lazy"/>
-      <img src="/images/img25.webp" alt="img25" className={styles.img25} loading="lazy"/>
-      <img src="/images/img26.webp" alt="img26" className={styles.img26} loading="lazy"/>
+      <img src="/images/dacapo.webp" alt="bg" className={styles.dacapo} />
+      <img src="/images/img1.webp" alt="img1" className={styles.img1} />
+      <img src="/images/img2.webp" alt="img2" className={styles.img2} />
+      <img src="/images/img3.webp" alt="img3" className={styles.img3} />
+      <img src="/images/img4.webp" alt="img4" className={styles.img4} />
+      <img src="/images/img5.webp" alt="img5" className={styles.img5} />
+      <img src="/images/img6.webp" alt="img6" className={styles.img6} />
+      <img src="/images/img7.webp" alt="img7" className={styles.img7} />
+      <img src="/images/img8.webp" alt="img8" className={styles.img8} />
+      <img src="/images/img9.webp" alt="img9" className={styles.img9} />
+      <img src="/images/img10.webp" alt="img10" className={styles.img10} />
+      <img src="/images/img11.webp" alt="img11" className={styles.img11} />
+      <img src="/images/img12.webp" alt="img12" className={styles.img12} />
+      <img src="/images/img13.webp" alt="img13" className={styles.img13} />
+      <img src="/images/img14.webp" alt="img14" className={styles.img14} />
+      <img src="/images/img15.webp" alt="img15" className={styles.img15} />
+      <img src="/images/img16.webp" alt="img16" className={styles.img16} />
+      <img src="/images/img17.webp" alt="img17" className={styles.img17} />
+      <img src="/images/img18.webp" alt="img18" className={styles.img18} />
+      <img src="/images/img19.webp" alt="img19" className={styles.img19} />
+      <img src="/images/img20.webp" alt="img20" className={styles.img20} />
+      <img src="/images/img21.webp" alt="img21" className={styles.img21} />
+      <img src="/images/img22.webp" alt="img22" className={styles.img22} />
+      <img src="/images/img23.webp" alt="img23" className={styles.img23} />
+      <img src="/images/img24.webp" alt="img24" className={styles.img24} />
+      <img src="/images/img25.webp" alt="img25" className={styles.img25} />
+      <img src="/images/img26.webp" alt="img26" className={styles.img26} />
     </div>
   );
 };
